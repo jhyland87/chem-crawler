4170aebab35e8d4f11025ae17b35b909e9c85415
Justin Hyland <jhyland87@gmail.com>
2025-05-08 08:46:53 -0700

    CLeaning up the factory a bit

--- src/supplier_factory.ts
+++ src/supplier_factory.ts
@@ -4,7 +4,7 @@ import * as suppliers from './suppliers'
 import SupplierBase from './suppliers/supplier_base'
 
 export default class SupplierFactory<T extends IProduct> implements AsyncIterable<T> {
-  public static supplier_list: Array<string> = Object.keys(suppliers)//.map(s => s.replace(/^Supplier/, ''))
+  public static supplier_list: Array<string> = Object.keys(suppliers)
 
   private _query: string
 
@@ -19,7 +19,7 @@ export default class SupplierFactory<T extends IProduct> implements AsyncIterabl
   }
 
   public static supplierList() {
-    return Object.keys(suppliers)//.map(s => s.replace(/^Supplier/, ''))
+    return Object.keys(suppliers)
   }
 
   async *[Symbol.asyncIterator](): AsyncGenerator<T, void, unknown> {
@@ -33,19 +33,12 @@ export default class SupplierFactory<T extends IProduct> implements AsyncIterabl
       }
 
       // Only iterate over the suppliers that are selected (or all if none are selected)
-
-      console.debug('suppliers', suppliers)
-      console.debug('this._suppliers', this._suppliers)
-      console.debug('filtered', Object.entries(suppliers)
-        .filter(([key, _]) => this._suppliers.length == 0 || this._suppliers.includes(key)))
-
-      const toIterateOver = Object.entries(suppliers)
-        .filter(([key, _]) => this._suppliers.length == 0 || this._suppliers.includes(key))
-        .map(([_, value]) => value)
-
-      console.debug('toIterateOver', toIterateOver)
-
-      const masterIterator = combineAsyncIterators(toIterateOver.map(s => new s(this._query, 10, this._controller)))
+      const masterIterator = combineAsyncIterators(
+        Object.entries(suppliers)
+          .filter(([key, _]) => this._suppliers.length == 0 || this._suppliers.includes(key))
+          .map(([_, value]) => value)
+          .map(s => new s(this._query, 10, this._controller))
+      )
 
       for await (const value of masterIterator) {
         yield value as T;
