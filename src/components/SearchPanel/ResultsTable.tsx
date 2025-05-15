312149a5ae789997df2471b7ad2990a501ecc93a
jhyland87 <jhyland87@gmail.com>
2025-05-14 17:09:27 -0700

    updates

--- src/components/SearchPanel/ResultsTable.tsx
+++ src/components/SearchPanel/ResultsTable.tsx
@@ -15,7 +15,7 @@ import {
 import { isEmpty } from "lodash";
 import { CSSProperties, Fragment, ReactElement, useEffect, useState } from "react";
 import { useSettings } from "../../context";
-import SupplierFactory from "../../suppliers/supplier.factory";
+import SupplierFactory from "../../suppliers/SupplierFactory";
 import { Product, ProductTableProps } from "../../types";
 import LoadingBackdrop from "../LoadingBackdrop";
 import Pagination from "./Pagination";
