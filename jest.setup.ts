347bded31cd511c564274aa5fd2a1f70670995d7
Justin Hyland <jhyland87@gmail.com>
2025-05-18 11:15:19 -0700

    Trying to get mocks working

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,16 +1,13 @@
 // Import necessary modules
 import "@testing-library/jest-dom";
-/*
 import jestChrome from "jest-chrome";
-import { Chrome } from "./src/types/chrome-storage";
-
-
 
 // Assign jest-chrome to the global object
 Object.assign(global, jestChrome);
-
+/*
 // Type assertion for the global chrome object
 const globalChrome = global as unknown as { chrome: Chrome };
+
 
 // Promisify chrome.storage.session.set and chrome.storage.session.get
 globalChrome.chrome.storage.session.set = util.promisify(
