b46e5348b92a5d9b91817e855b03b6467faffb1b
Justin Hyland <jhyland87@gmail.com>
2025-05-18 11:20:07 -0700

    Handlers

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -58,8 +58,3 @@ export const handlers = [
     }),
   ),
 ];
-
-//o=getFilteredProducts
-//s=WixStoresWebClient
-//q="query%2CgetFilteredProductsWithHasDiscount%28%24mainCollectionId%3AString%21%2C%24filters%3AProductFilters%2C%24sort%3AProductSort%2C%24offset%3AInt%2C%24limit%3AInt%2C%24withOptions%3ABoolean%2C%3D%2Cfalse%2C%24withPriceRange%3ABoolean%2C%3D%2Cfalse%29%7Bcatalog%7Bcategory%28categoryId%3A%24mainCollectionId%29%7BnumOfProducts%2CproductsWithMetaData%28filters%3A%24filters%2Climit%3A%24limit%2Csort%3A%24sort%2Coffset%3A%24offset%2ConlyVisible%3Atrue%29%7BtotalCount%2Clist%7Bid%2Coptions%7Bid%2Ckey%2Ctitle%2C%40include%28if%3A%24withOptions%29%2CoptionType%2C%40include%28if%3A%24withOptions%29%2Cselections%2C%40include%28if%3A%24withOptions%29%7Bid%2Cvalue%2Cdescription%2Ckey%2CinStock%7D%7DproductItems%2C%40include%28if%3A%24withOptions%29%7Bid%2CoptionsSelections%2Cprice%2CformattedPrice%7DproductType%2Cprice%2Csku%2CisInStock%2CurlPart%2CformattedPrice%2Cname%2Cdescription%2Cbrand%2CpriceRange%28withSubscriptionPriceRange%3Atrue%29%2C%40include%28if%3A%24withPriceRange%29%7BfromPriceFormatted%7D%7D%7D%7D%7D%7D"
-//v="%7B%22mainCollectionId%22%3A%2200000000-000000-000000-000000000001%22%2C%22offset%22%3A0%2C%22limit%22%3A10%2C%22sort%22%3Anull%2C%22filters%22%3A%7B%22term%22%3A%7B%22field%22%3A%22name%22%2C%22op%22%3A%22CONTAINS%22%2C%22values%22%3A%5B%22*acid*%22%5D%7D%7D%2C%22withOptions%22%3Atrue%2C%22withPriceRange%22%3Afalse%7D"
