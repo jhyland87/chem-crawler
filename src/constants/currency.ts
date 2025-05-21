9dff980c44f65197008fa9df18f018d89ec02089
Justin Hyland <jhyland87@gmail.com>
2025-05-21 07:11:22 -0700

    const

--- src/constants/currency.ts
+++ src/constants/currency.ts
@@ -71,7 +71,186 @@ export const CURRENCY_CODE_MAP: { [key: string]: string } = {
  * CurrencySymbolMap['EUR'] // returns '€'
  * ```
  */
-export const CURRENCY_SYMBOL_MAP: { [key: string]: string } = {
+
+/**
+ * Represents ISO 4217 currency codes supported by the application.
+ * Each code is a three-letter string that uniquely identifies a currency.
+ * For example, USD for US Dollar, EUR for Euro, GBP for British Pound.
+ */
+type CurrencyCodes =
+  | "AED" /** United Arab Emirates Dirham */
+  | "AFN" /** Afghan Afghani */
+  | "ALL" /** Albanian Lek */
+  | "AMD" /** Armenian Dram */
+  | "ANG" /** Netherlands Antillean Guilder */
+  | "AOA" /** Angolan Kwanza */
+  | "ARS" /** Argentine Peso */
+  | "AUD" /** Australian Dollar */
+  | "AWG" /** Aruban Florin */
+  | "AZN" /** Azerbaijani Manat */
+  | "BAM" /** Bosnia-Herzegovina Convertible Mark */
+  | "BBD" /** Barbadian Dollar */
+  | "BDT" /** Bangladeshi Taka */
+  | "BGN" /** Bulgarian Lev */
+  | "BHD" /** Bahraini Dinar */
+  | "BIF" /** Burundian Franc */
+  | "BMD" /** Bermudan Dollar */
+  | "BND" /** Brunei Dollar */
+  | "BOB" /** Bolivian Boliviano */
+  | "BRL" /** Brazilian Real */
+  | "BSD" /** Bahamian Dollar */
+  | "BTC" /** Bitcoin */
+  | "BTN" /** Bhutanese Ngultrum */
+  | "BWP" /** Botswanan Pula */
+  | "BYR" /** Belarusian Ruble (pre-2016) */
+  | "BYN" /** Belarusian Ruble */
+  | "BZD" /** Belize Dollar */
+  | "CAD" /** Canadian Dollar */
+  | "CDF" /** Congolese Franc */
+  | "CHF" /** Swiss Franc */
+  | "CLP" /** Chilean Peso */
+  | "CNY" /** Chinese Yuan */
+  | "COP" /** Colombian Peso */
+  | "CRC" /** Costa Rican Colón */
+  | "CUC" /** Cuban Convertible Peso */
+  | "CUP" /** Cuban Peso */
+  | "CVE" /** Cape Verdean Escudo */
+  | "CZK" /** Czech Republic Koruna */
+  | "DJF" /** Djiboutian Franc */
+  | "DKK" /** Danish Krone */
+  | "DOP" /** Dominican Peso */
+  | "DZD" /** Algerian Dinar */
+  | "EEK" /** Estonian Kroon */
+  | "EGP" /** Egyptian Pound */
+  | "ERN" /** Eritrean Nakfa */
+  | "ETB" /** Ethiopian Birr */
+  | "ETH" /** Ethereum */
+  | "EUR" /** Euro */
+  | "FJD" /** Fijian Dollar */
+  | "FKP" /** Falkland Islands Pound */
+  | "GBP" /** British Pound Sterling */
+  | "GEL" /** Georgian Lari */
+  | "GGP" /** Guernsey Pound */
+  | "GHC" /** Ghanaian Cedi (pre-2007) */
+  | "GHS" /** Ghanaian Cedi */
+  | "GIP" /** Gibraltar Pound */
+  | "GMD" /** Gambian Dalasi */
+  | "GNF" /** Guinean Franc */
+  | "GTQ" /** Guatemalan Quetzal */
+  | "GYD" /** Guyanaese Dollar */
+  | "HKD" /** Hong Kong Dollar */
+  | "HNL" /** Honduran Lempira */
+  | "HRK" /** Croatian Kuna */
+  | "HTG" /** Haitian Gourde */
+  | "HUF" /** Hungarian Forint */
+  | "IDR" /** Indonesian Rupiah */
+  | "ILS" /** Israeli New Sheqel */
+  | "IMP" /** Isle of Man Pound */
+  | "INR" /** Indian Rupee */
+  | "IQD" /** Iraqi Dinar */
+  | "IRR" /** Iranian Rial */
+  | "ISK" /** Icelandic Króna */
+  | "JEP" /** Jersey Pound */
+  | "JMD" /** Jamaican Dollar */
+  | "JOD" /** Jordanian Dinar */
+  | "JPY" /** Japanese Yen */
+  | "KES" /** Kenyan Shilling */
+  | "KGS" /** Kyrgystani Som */
+  | "KHR" /** Cambodian Riel */
+  | "KMF" /** Comorian Franc */
+  | "KPW" /** North Korean Won */
+  | "KRW" /** South Korean Won */
+  | "KYD" /** Cayman Islands Dollar */
+  | "KZT" /** Kazakhstani Tenge */
+  | "LAK" /** Laotian Kip */
+  | "LBP" /** Lebanese Pound */
+  | "LKR" /** Sri Lankan Rupee */
+  | "LRD" /** Liberian Dollar */
+  | "LSL" /** Lesotho Loti */
+  | "LTL" /** Lithuanian Litas */
+  | "LVL" /** Latvian Lats */
+  | "LYD" /** Libyan Dinar */
+  | "MAD" /** Moroccan Dirham */
+  | "MDL" /** Moldovan Leu */
+  | "MGA" /** Malagasy Ariary */
+  | "MKD" /** Macedonian Denar */
+  | "MMK" /** Myanma Kyat */
+  | "MNT" /** Mongolian Tugrik */
+  | "MOP" /** Macanese Pataca */
+  | "MRO" /** Mauritanian Ouguiya (pre-2018) */
+  | "MRU" /** Mauritanian Ouguiya */
+  | "MUR" /** Mauritian Rupee */
+  | "MVR" /** Maldivian Rufiyaa */
+  | "MWK" /** Malawian Kwacha */
+  | "MXN" /** Mexican Peso */
+  | "MYR" /** Malaysian Ringgit */
+  | "MZN" /** Mozambican Metical */
+  | "NAD" /** Namibian Dollar */
+  | "NGN" /** Nigerian Naira */
+  | "NIO" /** Nicaraguan Córdoba */
+  | "NOK" /** Norwegian Krone */
+  | "NPR" /** Nepalese Rupee */
+  | "NZD" /** New Zealand Dollar */
+  | "OMR" /** Omani Rial */
+  | "PAB" /** Panamanian Balboa */
+  | "PEN" /** Peruvian Nuevo Sol */
+  | "PGK" /** Papua New Guinean Kina */
+  | "PHP" /** Philippine Peso */
+  | "PKR" /** Pakistani Rupee */
+  | "PLN" /** Polish Złoty */
+  | "PYG" /** Paraguayan Guarani */
+  | "QAR" /** Qatari Rial */
+  | "RON" /** Romanian Leu */
+  | "RSD" /** Serbian Dinar */
+  | "RWF" /** Rwandan Franc */
+  | "SAR" /** Saudi Riyal */
+  | "SBD" /** Solomon Islands Dollar */
+  | "SCR" /** Seychellois Rupee */
+  | "SDG" /** Sudanese Pound */
+  | "SEK" /** Swedish Krona */
+  | "SGD" /** Singapore Dollar */
+  | "SHP" /** Saint Helena Pound */
+  | "SLL" /** Sierra Leonean Leone */
+  | "SOS" /** Somali Shilling */
+  | "SRD" /** Surinamese Dollar */
+  | "SSP" /** South Sudanese Pound */
+  | "STD" /** São Tomé and Príncipe Dobra (pre-2018) */
+  | "STN" /** São Tomé and Príncipe Dobra */
+  | "SVC" /** Salvadoran Colón */
+  | "SYP" /** Syrian Pound */
+  | "SZL" /** Swazi Lilangeni */
+  | "THB" /** Thai Baht */
+  | "TJS" /** Tajikistani Somoni */
+  | "TMT" /** Turkmenistani Manat */
+  | "TND" /** Tunisian Dinar */
+  | "TOP" /** Tongan Paʻanga */
+  | "TRY" /** Turkish Lira */
+  | "TTD" /** Trinidad and Tobago Dollar */
+  | "TVD" /** Tuvaluan Dollar */
+  | "TWD" /** New Taiwan Dollar */
+  | "TZS" /** Tanzanian Shilling */
+  | "UAH" /** Ukrainian Hryvnia */
+  | "UGX" /** Ugandan Shilling */
+  | "USD" /** United States Dollar */
+  | "UYU" /** Uruguayan Peso */
+  | "UZS" /** Uzbekistan Som */
+  | "VEF" /** Venezuelan Bolívar */
+  | "VND" /** Vietnamese Dong */
+  | "VUV" /** Vanuatu Vatu */
+  | "WST" /** Samoan Tala */
+  | "XAF" /** CFA Franc BEAC */
+  | "XBT" /** Bitcoin */
+  | "XCD" /** East Caribbean Dollar */
+  | "XOF" /** CFA Franc BCEAO */
+  | "XPF" /** CFP Franc */
+  | "YER" /** Yemeni Rial */
+  | "ZAR" /** South African Rand */
+  | "ZWD" /** Zimbabwean Dollar */
+  | "LTC" /** Litecoin */
+  | "RMB" /** Chinese Yuan Renminbi */
+  | "TRL" /** Turkish Lira */;
+
+export const CURRENCY_SYMBOL_MAP: { [key in CurrencyCodes]: string } = {
   AED: "د.إ",
   AFN: "؋",
   ALL: "L",
