e2e4e8028931b05ac22b5ec7a87eee051faa37c5
Justin Hyland <jhyland87@gmail.com>
2025-06-15 11:08:20 -0700

    Moving styles to Styles.ts

--- src/components/SearchPanel/FilterMenu.tsx
+++ src/components/SearchPanel/FilterMenu.tsx
@@ -1,18 +1,19 @@
 import ArrowForwardIosSharpIcon from "@/icons/ArrowDropDownIcon";
-import MuiAccordion, { AccordionProps } from "@mui/material/Accordion";
-import MuiAccordionDetails from "@mui/material/AccordionDetails";
-import MuiAccordionSummary, {
-  accordionSummaryClasses,
-  AccordionSummaryProps,
-} from "@mui/material/AccordionSummary";
 import Box from "@mui/material/Box";
-import Drawer from "@mui/material/Drawer";
-import { styled } from "@mui/material/styles";
 import Tab from "@mui/material/Tab";
-import Tabs from "@mui/material/Tabs";
 import Typography from "@mui/material/Typography";
 import * as React from "react";
 import { forwardRef, Ref, useImperativeHandle, useState, type SyntheticEvent } from "react";
+import {
+  FilterMenuAccordion,
+  FilterMenuAccordionDetails,
+  FilterMenuAccordionSummary,
+  FilterMenuBorder,
+  FilterMenuDrawer,
+  FilterMenuDrawerContent,
+  FilterMenuTabs,
+  FilterMenuTabsContainer,
+} from "../Styles";
 
 type FilterMenuRef = {
   toggleDrawer: (open: boolean) => void;
@@ -51,52 +52,6 @@ function a11yProps(index: number) {
   };
 }
 
-const Accordion = styled((props: AccordionProps) => (
-  <MuiAccordion disableGutters elevation={0} square {...props} />
-))(({ theme }) => ({
-  border: `1px solid ${theme.palette.divider}`,
-  // eslint-disable-next-line @typescript-eslint/naming-convention
-  "&:not(:last-child)": {
-    borderBottom: 0,
-  },
-  // eslint-disable-next-line @typescript-eslint/naming-convention
-  "&::before": {
-    display: "none",
-  },
-}));
-
-const AccordionSummary = styled((props: AccordionSummaryProps) => (
-  <MuiAccordionSummary
-    sx={{
-      display: "flex",
-      minHeight: "28px",
-      maxHeight: "48px",
-      overflowY: "scroll",
-      minWidth: "40px",
-    }}
-    expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: "0.9rem" }} />}
-    {...props}
-  />
-))(({ theme }) => ({
-  backgroundColor: "rgba(0, 0, 0, .03)",
-  flexDirection: "row-reverse",
-  [`& .${accordionSummaryClasses.expandIconWrapper}.${accordionSummaryClasses.expanded}`]: {
-    transform: "rotate(90deg)",
-  },
-  [`& .${accordionSummaryClasses.content}`]: {
-    margin: "1px 0px 1px 0px",
-    marginLeft: theme.spacing(1),
-  },
-  ...theme.applyStyles("dark", {
-    backgroundColor: "rgba(255, 255, 255, .05)",
-  }),
-}));
-
-const AccordionDetails = styled(MuiAccordionDetails)(() => ({
-  padding: 0,
-  borderTop: "1px solid rgba(0, 0, 0, .125)",
-}));
-
 function CustomizedAccordions() {
   const [expanded, setExpanded] = useState<string | false>("panel1");
 
@@ -106,30 +61,60 @@ function CustomizedAccordions() {
 
   return (
     <div>
-      <Accordion expanded={expanded === "panel1"} onChange={handleChange("panel1")}>
-        <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
+      <FilterMenuAccordion
+        expanded={expanded === "panel1"}
+        onChange={handleChange("panel1")}
+        disableGutters
+        elevation={0}
+        square
+      >
+        <FilterMenuAccordionSummary
+          aria-controls="panel1d-content"
+          id="panel1d-header"
+          expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: "0.9rem" }} />}
+        >
           <Typography component="span">Athlete</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
+        </FilterMenuAccordionSummary>
+        <FilterMenuAccordionDetails>
           <div>Search and filter options for athletes will go here.</div>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion expanded={expanded === "panel2"} onChange={handleChange("panel2")}>
-        <AccordionSummary aria-controls="panel2d-content" id="panel2d-header">
+        </FilterMenuAccordionDetails>
+      </FilterMenuAccordion>
+      <FilterMenuAccordion
+        expanded={expanded === "panel2"}
+        onChange={handleChange("panel2")}
+        disableGutters
+        elevation={0}
+        square
+      >
+        <FilterMenuAccordionSummary
+          aria-controls="panel2d-content"
+          id="panel2d-header"
+          expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: "0.9rem" }} />}
+        >
           <Typography component="span">Country</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
+        </FilterMenuAccordionSummary>
+        <FilterMenuAccordionDetails>
           <div>Search and filter options for countries will go here.</div>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion expanded={expanded === "panel3"} onChange={handleChange("panel3")}>
-        <AccordionSummary aria-controls="panel3d-content" id="panel3d-header">
+        </FilterMenuAccordionDetails>
+      </FilterMenuAccordion>
+      <FilterMenuAccordion
+        expanded={expanded === "panel3"}
+        onChange={handleChange("panel3")}
+        disableGutters
+        elevation={0}
+        square
+      >
+        <FilterMenuAccordionSummary
+          aria-controls="panel3d-content"
+          id="panel3d-header"
+          expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: "0.9rem" }} />}
+        >
           <Typography component="span">Sport</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
+        </FilterMenuAccordionSummary>
+        <FilterMenuAccordionDetails>
           <div>Search and filter options for sports will go here.</div>
-        </AccordionDetails>
-      </Accordion>
+        </FilterMenuAccordionDetails>
+      </FilterMenuAccordion>
     </div>
   );
 }
@@ -164,7 +149,7 @@ function FilterMenu(props: object, ref: Ref<FilterMenuRef>) {
   };
 
   const drawerContent = () => (
-    <Box sx={{ width: 300, padding: "0px" }} role="presentation">
+    <FilterMenuDrawerContent role="presentation">
       <TabPanel value={activeTab} index={0} style={{ padding: 0 }}>
         <CustomizedAccordions />
       </TabPanel>
@@ -178,7 +163,7 @@ function FilterMenu(props: object, ref: Ref<FilterMenuRef>) {
           </ul>
         </div>
       </TabPanel>
-    </Box>
+    </FilterMenuDrawerContent>
   );
 
   useImperativeHandle(ref, () => ({
@@ -189,82 +174,25 @@ function FilterMenu(props: object, ref: Ref<FilterMenuRef>) {
   return (
     <>
       {/* Continuous right border/frame */}
-      <Box
-        sx={{
-          position: "fixed",
-          right: 0,
-          top: 0,
-          bottom: 0,
-          width: "33px",
-          bgcolor: "background.paper",
-          borderLeft: "1px solid #e0e0e0", // Match the tab border color
-          zIndex: 1400, // Above drawer backdrop
-        }}
-      />
+      <FilterMenuBorder />
 
       {/* Fixed tabs on the right side */}
-      <Box
-        sx={{
-          position: "fixed",
-          right: 0,
-          top: "17%",
-          transform: "translateY(-20%)",
-          zIndex: 1500, // Higher than backdrop and border
-          bgcolor: "background.paper",
-          borderTop: "1px solid #e0e0e0", // Light grey border
-          borderBottom: "1px solid #e0e0e0",
-          // Remove border radius for sharp 90° angles
-        }}
-      >
-        <Tabs
+      <FilterMenuTabsContainer>
+        <FilterMenuTabs
           orientation="vertical"
           value={activeTab}
           onChange={handleTabChange}
           aria-label="Filter menu tabs"
-          sx={{
-            minWidth: "32px",
-            width: "32px",
-            bgcolor: "background.paper", // Ensure background matches
-            // eslint-disable-next-line @typescript-eslint/naming-convention
-            "& .MuiTab-root": {
-              minWidth: "33px",
-              minHeight: "60px",
-              padding: "8px 4px",
-              writingMode: "vertical-rl",
-              textOrientation: "upright",
-              fontSize: "0.75rem",
-              bgcolor: "background.paper", // Match background
-              borderBottom: "1px solid #e0e0e0", // Subtle separator between tabs
-              // eslint-disable-next-line @typescript-eslint/naming-convention
-              "&:last-child": {
-                borderBottom: "none", // Remove border from last tab
-              },
-              // eslint-disable-next-line @typescript-eslint/naming-convention
-              "&.Mui-selected": {
-                bgcolor: "action.selected", // Slightly different background for selected tab
-              },
-            },
-          }}
         >
           <Tab label="Filters" {...a11yProps(0)} onClick={handleTabClick} />
           <Tab label="Sort" {...a11yProps(1)} onClick={handleTabClick} />
-        </Tabs>
-      </Box>
+        </FilterMenuTabs>
+      </FilterMenuTabsContainer>
 
       {/* Drawer that slides out from the tabs */}
-      <Drawer
-        anchor="right"
-        open={state.right}
-        onClose={() => toggleDrawer(false)}
-        sx={{
-          // eslint-disable-next-line @typescript-eslint/naming-convention
-          "& .MuiDrawer-paper": {
-            marginRight: "33px", // Account for thinner tab width
-          },
-        }}
-      >
+      <FilterMenuDrawer anchor="right" open={state.right} onClose={() => toggleDrawer(false)}>
         {drawerContent()}
-      </Drawer>
+      </FilterMenuDrawer>
     </>
   );
 }
--- src/components/Styles.ts
+++ src/components/Styles.ts
@@ -1,6 +1,12 @@
 /* eslint-disable @typescript-eslint/naming-convention */
+import MuiAccordion from "@mui/material/Accordion";
+import MuiAccordionDetails from "@mui/material/AccordionDetails";
+import MuiAccordionSummary from "@mui/material/AccordionSummary";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
 import FormControl, { FormControlProps } from "@mui/material/FormControl";
 import { styled } from "@mui/material/styles";
+import Tabs from "@mui/material/Tabs";
 
 export const StyledFormControlSelector = styled(FormControl)<FormControlProps>(({ theme }) => ({
   color: theme.palette.primary.dark,
@@ -36,6 +42,97 @@ export const StyledFormControlSelector = styled(FormControl)<FormControlProps>((
       fontSize: 14,
       //marginTop: "-7px",
     },
+}));
+
+// FilterMenu Styled Components
+export const FilterMenuBorder = styled(Box)(({ theme }) => ({
+  position: "fixed",
+  right: 0,
+  top: 0,
+  bottom: 0,
+  width: "33px",
+  backgroundColor: theme.palette.background.paper,
+  borderLeft: "1px solid #e0e0e0",
+  zIndex: 1400,
+}));
+
+export const FilterMenuTabsContainer = styled(Box)(({ theme }) => ({
+  position: "fixed",
+  right: 0,
+  top: "17%",
+  transform: "translateY(-20%)",
+  zIndex: 1500,
+  backgroundColor: theme.palette.background.paper,
+  borderTop: "1px solid #e0e0e0",
+  borderBottom: "1px solid #e0e0e0",
+}));
+
+export const FilterMenuTabs = styled(Tabs)(({ theme }) => ({
+  minWidth: "32px",
+  width: "32px",
+  backgroundColor: theme.palette.background.paper,
+  "& .MuiTab-root": {
+    minWidth: "33px",
+    minHeight: "60px",
+    padding: "8px 4px",
+    writingMode: "vertical-rl",
+    textOrientation: "upright",
+    fontSize: "0.75rem",
+    backgroundColor: theme.palette.background.paper,
+    borderBottom: "1px solid #e0e0e0",
+    "&:last-child": {
+      borderBottom: "none",
+    },
+    "&.Mui-selected": {
+      backgroundColor: theme.palette.action.selected,
+    },
+  },
+}));
+
+export const FilterMenuDrawer = styled(Drawer)(() => ({
+  "& .MuiDrawer-paper": {
+    marginRight: "33px",
+  },
+}));
+
+export const FilterMenuDrawerContent = styled(Box)(() => ({
+  width: 300,
+  padding: "0px",
+}));
+
+export const FilterMenuAccordion = styled(MuiAccordion)(({ theme }) => ({
+  border: `1px solid ${theme.palette.divider}`,
+  "&:not(:last-child)": {
+    borderBottom: 0,
+  },
+  "&::before": {
+    display: "none",
+  },
+}));
+
+export const FilterMenuAccordionSummary = styled(MuiAccordionSummary)(({ theme }) => ({
+  display: "flex",
+  minHeight: "28px",
+  maxHeight: "48px",
+  overflowY: "scroll",
+  minWidth: "40px",
+  backgroundColor: "rgba(0, 0, 0, .03)",
+  flexDirection: "row-reverse",
+  "& .MuiAccordionSummary-expandIconWrapper.Mui-expanded": {
+    transform: "rotate(90deg)",
+  },
+  "& .MuiAccordionSummary-content": {
+    margin: "1px 0px 1px 0px",
+    marginLeft: theme.spacing(1),
+  },
+  ...theme.applyStyles("dark", {
+    backgroundColor: "rgba(255, 255, 255, .05)",
+  }),
+}));
+
+export const FilterMenuAccordionDetails = styled(MuiAccordionDetails)(() => ({
+  padding: 0,
+  borderTop: "1px solid rgba(0, 0, 0, .125)",
 }));
 
 //MuiFormLabel-root MuiInputLabel-root MuiInputLabel-formControl MuiInputLabel-animated MuiInputLabel-outlined MuiFormLabel-colorPrimary MuiInputLabel-root MuiInputLabel-formControl MuiInputLabel-animated MuiInputLabel-outlined css-enqxln-MuiFormLabel-root-MuiInputLabel-root
