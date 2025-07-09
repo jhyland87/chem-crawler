08a457ea465ddd15733f7917049e5ef13a0aa2c8
Justin Hyland <jhyland87@gmail.com>
2025-07-09 08:26:28 -0700

    Adding Pubchem to util index

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,4 +2,5 @@ export { default as BadgeAnimator } from "./BadgeAnimator";
 export { default as Cactus } from "./Cactus";
 export { default as Logger } from "./Logger";
 export { default as ProductBuilder } from "./ProductBuilder";
+export { default as Pubchem } from "./Pubchem";
 export { default as SupplierCache } from "./SupplierCache";
